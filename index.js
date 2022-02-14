45f6f8fbbbb1a8ee6aa1cb4004706a9ed1497b68
access6080 <access.6080@gmail.com>
2022-02-14 15:13:37 -0500

    deploy

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ import chatRoutes from './routes/messages.js'
 
 const app = express();
 app.use(cookieParser());
+app.use(express.static("build"))
 app.use(cors({
-    origin: 'https://jeschat.geokoranteng.com',
+    origin: 'http://localhost:3000',
     credentials:true,       
     optionSuccessStatus:200
 }));
@@ -52,6 +53,8 @@ app.use(express.json());
 app.use('/auth/', userRoutes);
 app.use('/chat/', chatRoutes);
 
+const rooms = {};
+
 // Initailize Socket Connection
 io.on('connection', (socket) => {
     socket.on('joinChat', ({ username, room }) => {
@@ -63,16 +66,49 @@ io.on('connection', (socket) => {
 
     socket.on('newMessage', (data) => {
         const user = getCurrentUser(socket.id);
-        io.to(user.room).emit('newChat', {username: user.username, message: data.message})
-    })
+        io.to(user.room).emit('newChat', { username: user.username, message: data.message })
+    });
 
+    
     socket.on('disconnect', () => {
         const user = userLeave(socket.id);
 
         if (user) {
-            io.to(user.room).emit('newChat',{username: 'ADMIN', message: `${capitalize(user.username)} has left the chat`});
+            io.to(user.room).emit('newChat', { username: 'ADMIN', message: `${capitalize(user.username)} has left the chat` });
         }
-    })
+    });
+
+    // Video Call
+    socket.on("join room", roomID => {
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+
+        const otherUser = rooms[roomID].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
+        }
+    });
+
+    socket.on("offer", payload => {
+        io.to(payload.target).emit("offer", payload);
+    });
+
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload);
+    });
+
+    socket.on("ice-candidate", incoming => {
+        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+    });
+
+
+    // socket.on('disconnect', () => {
+    //     socket.broadcast.emit("user left");
+    // })
 
 });
 
